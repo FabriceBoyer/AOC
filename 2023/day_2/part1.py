5bc643f7e17880f17b4e3f3519471849b8ee86a5
Fabrice Boyer <3055419+FabriceBoyer@users.noreply.github.com>
2023-12-03 15:03:26 +0100

    day_2

---
+++ 2023/day_2/part1.py
@@ -0,0 +1,26 @@
+sum = 0
+with open("input.txt") as f:
+    games = [line.strip() for line in f.readlines()]
+    for game in games:
+        game_split = game.split(":")
+        game_id = int(game_split[0].replace("Game", ""))
+        draws = game_split[1].split(";")
+        possible = True
+        for draw in draws:
+            counts_and_colors = draw.split(",")
+            for count_and_color in counts_and_colors:
+                count_and_color_split = count_and_color.strip().split(" ")
+                count = int(count_and_color_split[0])
+                color = count_and_color_split[1]
+                if color == "red" and count > 12:
+                    possible = False
+                    break
+                if color == "green" and count > 13:
+                    possible = False
+                    break
+                if color == "blue" and count > 14:
+                    possible = False
+                    break
+        if possible:
+            sum += game_id
+print(sum)
