24e4177b5a16c795eeee07cac53021e4c7aa3b4c
Fabrice Boyer <3055419+FabriceBoyer@users.noreply.github.com>
2023-12-05 10:45:49 +0100

    day_1

---
+++ 2023/day_1/day_1_part1.py
@@ -0,0 +1,21 @@
+import os
+
+with open("day_1/input.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+sum = 0
+for line in lines:
+    char1Set = False
+    for char in line:
+        if char.isnumeric():
+            if not char1Set:
+                char1 = char
+                char1Set = True
+                char2 = char1
+            else:
+                char2 = char
+    calibValue = int(char1 + char2)
+    print(f"Calib value of '{line}' is '{calibValue}'")
+    sum += calibValue
+
+print(f"Sum:  {sum}")
---
+++ 2023/day_1/day_1_part2.py
@@ -0,0 +1,57 @@
+import os
+
+textNumberArray = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
+
+
+def getTextNumberEndValue(str) -> int:
+    i = 0
+    for textNumber in textNumberArray:
+        i += 1
+        if str.endswith(textNumber):
+            return i
+    return -1
+
+
+with open("day_1/input.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+sum = 0
+for line in lines:
+    char1Set = False
+    partialTextNumber = ""
+    for char in line:
+        if char.isnumeric():  # num
+            partialTextNumber = ""  # reset
+            if not char1Set:
+                char1 = char
+                char1Set = True
+                char2 = char1
+            else:
+                char2 = char
+        else:  # alpha
+            partialTextNumber += char
+            number = getTextNumberEndValue(partialTextNumber)
+            if number > 0:
+                partialTextNumber = char
+                if not char1Set:
+                    char1 = str(number)
+                    char1Set = True
+                    char2 = char1
+                else:
+                    char2 = str(number)
+
+    calibValue = int(char1 + char2)
+    print(f"Calib value of '{line}' is '{calibValue}'")
+    sum += calibValue
+
+print(f"Sum:  {sum}")
