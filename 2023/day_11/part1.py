2bd1acd04fe22880442a66eddbdcfe984d3e5591
Fabrice Boyer <3055419+FabriceBoyer@users.noreply.github.com>
2023-12-11 13:21:59 +0000

    part 1 fix

--- 2023/day_11/part1.py
+++ 2023/day_11/part1.py
@@ -1,34 +1,41 @@
 from __future__ import annotations
 from dataclasses import dataclass
-import sys
+from typing import Optional
+import numpy as np
 
-with open("input.txt", encoding="utf-8") as f:
+with open("input2.txt", encoding="utf-8") as f:
     data = [line.strip() for line in f.readlines()]
 
 
 @dataclass
 class Galaxy:
+    orig_x: int
     x: int
     y: int
 
 
 y = 0
 galaxies: list[Galaxy] = []
+row_galaxy_count=np.zeros(len(data[0]))
 for line in data:
     empty_line = True
     for x, char in enumerate(line.strip()):
         if char == "#":
+            row_galaxy_count[x]+=1
             empty_line = False
-            galaxies.append(Galaxy(x, y))
-    y += 2 if empty_line else 1  # expansion
+            galaxies.append(Galaxy(x, x, y))
+    y += 2 if empty_line else 1  # y expansion
+
+# x expansion
+for i, r in enumerate(row_galaxy_count):
+    if r==0:
+        for g in galaxies:
+            if g.orig_x > i:
+                g.x+=1
 
 sum = 0
-count = len(galaxies)
-for i1, g1 in enumerate(galaxies[:-1]):
-    shortest_dist = sys.maxsize
-    i2 = i1 + 1
-    for g2 in galaxies[i2:]:
-        dist = abs(g2.x - g1.x) + abs(g2.y - g1.y)
-        shortest_dist = min(dist, shortest_dist)
-    sum += shortest_dist
+for i1, g1 in enumerate(galaxies):
+    for g2 in galaxies[i1+1:]:    
+        dist = abs(g2.x - g1.x) + abs(g2.y - g1.y)    
+        sum += dist    
 print(sum)
