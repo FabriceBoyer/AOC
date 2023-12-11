271e77c1596783783edd83bcc6e99ad242450f42
Fabrice Boyer <3055419+FabriceBoyer@users.noreply.github.com>
2023-12-11 19:19:26 +0100

    cleanup

--- 2023/day_11/part2.py
+++ 2023/day_11/part2.py
@@ -1,42 +1,41 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import Optional
 import numpy as np
 
 
 @dataclass
 class Galaxy:
-    orig_x: float
-    x: float
-    y: float
+    orig_x: int
+    x: int
+    y: int
 
 
 with open("input.txt", encoding="utf-8") as f:
     data = [line.strip() for line in f.readlines()]
 
-factor = 1e6
+factor: int = int(1e6)
 
 # parse
-y = 0
+y: int = 0
 galaxies: list[Galaxy] = []
-row_galaxy_count = np.zeros(len(data[0]))
+col_galaxy_count = np.zeros(len(data[0]))
 for line in data:
-    empty_line = True
+    empty_line: bool = True
     for x, char in enumerate(line.strip()):
         if char == "#":
-            row_galaxy_count[x] += 1
+            col_galaxy_count[x] += 1
             empty_line = False
             galaxies.append(Galaxy(x, x, y))
     y += factor if empty_line else 1  # y expansion
 
 # x expansion
-for i, r in enumerate(row_galaxy_count):
+for i, r in enumerate(col_galaxy_count):
     if r == 0:
         for g in galaxies:
             if g.orig_x > i:
                 g.x += factor - 1
 
-sum = 0
+sum: int = 0
 for i1, g1 in enumerate(galaxies):
     for g2 in galaxies[i1 + 1 :]:
         dist = abs(g2.x - g1.x) + abs(g2.y - g1.y)
