ba254946fd1a24a9a984741ffbffe84ee0d30db6
Fabrice Boyer <3055419+FabriceBoyer@users.noreply.github.com>
2023-12-11 13:22:32 +0000

    format

--- 2023/day_11/part1.py
+++ 2023/day_11/part1.py
@@ -16,26 +16,26 @@ class Galaxy:
 
 y = 0
 galaxies: list[Galaxy] = []
-row_galaxy_count=np.zeros(len(data[0]))
+row_galaxy_count = np.zeros(len(data[0]))
 for line in data:
     empty_line = True
     for x, char in enumerate(line.strip()):
         if char == "#":
-            row_galaxy_count[x]+=1
+            row_galaxy_count[x] += 1
             empty_line = False
             galaxies.append(Galaxy(x, x, y))
     y += 2 if empty_line else 1  # y expansion
 
 # x expansion
 for i, r in enumerate(row_galaxy_count):
-    if r==0:
+    if r == 0:
         for g in galaxies:
             if g.orig_x > i:
-                g.x+=1
+                g.x += 1
 
 sum = 0
 for i1, g1 in enumerate(galaxies):
-    for g2 in galaxies[i1+1:]:    
-        dist = abs(g2.x - g1.x) + abs(g2.y - g1.y)    
-        sum += dist    
+    for g2 in galaxies[i1 + 1 :]:
+        dist = abs(g2.x - g1.x) + abs(g2.y - g1.y)
+        sum += dist
 print(sum)
