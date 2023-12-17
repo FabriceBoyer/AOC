24444a701dd0a86592942be002e2412f6312a3ed
Fabrice Boyer <3055419+FabriceBoyer@users.noreply.github.com>
2023-12-17 01:11:23 +0100

    pattern detection

--- 2023/day_14/part2.py
+++ 2023/day_14/part2.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 import numpy as np
 
-with open("input2.txt", encoding="utf-8") as f:
+with open("input.txt", encoding="utf-8") as f:
     lines = [line.strip() for line in f.readlines()]
 
 line_count = len(lines)
 col_count = len(lines[0])
 platform = np.zeros((line_count, col_count), dtype="U1")
 stopper_offset = np.zeros(col_count, dtype=int)
+cycle_history: list[np.ndarray] = []
 
 
 def parse():
@@ -28,14 +29,32 @@ def tilt_north():
                 stopper_offset[x] += 1
 
 
-# detect repeated cycle pattern and apply modulo
+# -1 not found
+def inList(searched_elem, elem_list) -> int:
+    for i, elem in enumerate(elem_list):
+        if np.array_equal(elem, searched_elem):
+            return i
+    return -1
+
+
 def cycle(count: int):
     global platform
-    for i in range(count):
+    for counter in range(count):
         # Each cycle tilts the platform four times so that the rounded rocks roll north, then west, then south, then east.
         for _ in range(4):
             tilt_north()
-            platform = np.rot90(platform, k=-1)
+            platform = np.rot90(
+                platform, k=-1
+            )  # rotate and tilt north to compute all directions
+        i: int = inList(platform, cycle_history)
+        if i >= 0:
+            # detect repeated cycle pattern and apply modulo in range
+            print(
+                f"pattern found after {len(cycle_history)} cycles between cycle {counter} and {i}"
+            )
+            platform = cycle_history[i + (count - i - 1) % (counter - i)]
+            return
+        cycle_history.append(platform.copy())
 
 
 def compute_weight() -> int:
@@ -43,7 +62,7 @@ def compute_weight() -> int:
     for y, line in enumerate(platform):
         for char in line:
             if char == "O":
-                weight += y
+                weight += line_count - y
     return weight
 
 
@@ -53,7 +72,7 @@ def print_platform():
 
 
 parse()
-cycle(3)
+cycle(int(1e9))
 print_platform()
 weight = compute_weight()
 print(weight)
